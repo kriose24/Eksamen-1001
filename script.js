0241bc54ab02fae0fc18eccce47c3b2fba66991b
kriose24 <kristine.oz@outlook.com>
2025-06-01 16:58:29 +0200

    script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+    // DOM elements
+    const todoForm = document.getElementById('todo-form');
+    const todoInput = document.getElementById('todo-input');
+    const todoList = document.getElementById('todo-list');
